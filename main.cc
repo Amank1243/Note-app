3fedf610fd77ffa4ca43d7c37e2e92d400d428b1
Aman Khajuria <24khajuriaa@gmail.com>
2025-05-23 17:20:20 -0700

    Added exit feature

--- main.cc
+++ main.cc
@@ -8,10 +8,20 @@ using json = nlohmann::json;
 int main(int argc, char* argv[]) {
     std::cout << "Welcome to your task manager!\n";
     std::cout << "===============================\n";
+    std::cout << "Type any of the following to do an action: \n";
+    std::cout << "Type 'exit' to leave\n";
     std::cout << "What do you want to do?\n";
 
     std::string input;
     std::cin >> input;
+
+    if (input == "exit" || input == "Exit") {
+        return 0;
+    } else {
+        if (input == "add" || "Add") {
+
+        }
+    }
 
     /*
     json j;
