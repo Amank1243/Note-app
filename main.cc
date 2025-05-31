993db3059c2153d2567cab7503404677f0d5a457
Aman Khajuria <24khajuriaa@gmail.com>
2025-05-31 11:24:03 -0700

    Added "add task" feat; Fixed task ignored The ID was not working, because the getline while loop would have its cursor at the end of the file, due to the file being std::ios::app. To fix this, used seekg to set position at the begging rather than the end. I than incremented a counter to represent the ID for the task.

--- main.cc
+++ main.cc
@@ -1,27 +1,54 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <filesystem>
 
 int main(int argc, char* argv[]) {
 
-    // creat e filesystem
-    std::fstream file;
 
-    // turn c string into a string
+    // CHANGE c strings into a string
 
-    std::string arg1 = argv[1];
+    std::string action = argv[1];
+    std::string task = argv[2];
 
-    // Instructions in the README.md file will explain how to use the program
+    // NOTE: Instructions in the README.md file will explain how to use the program
 
-    // If commandline argument EQUALS "add", or "update", or "delete", run something
-    if (arg1 == "add") {
-        file.open("tasks.json");
-        file << argv[1];
-        std::cout << "adding\n";
-    } else if (arg1 == "update") {
+    // IF commandline argument EQUALS "add", or "update", or "delete", run something
+    if (action == "add") {
+        // Create an input stream to a json file
+        std::fstream file{"tasks.json", std::ios::in | std::ios::out | std::ios::app};
 
-    } else if (arg1 == "delete") {
+        // IF file is open, store task in it, else throw an error
+        if (file.is_open()) {
+            file << task;
+            file << "\n";
+        } else {
+            std::cerr << "Error: File failed to open";
+        }
+
+        // GET task ID
+        int lines = 1;
+        std::string line;
+        // Reset the file cursor position, otherwise the while loop would start at the very end
+        file.seekg(0, std::ios::beg);
+        while (std::getline(file, line)) {
+            if (!file.is_open()) {
+                std::cerr << "Error: File failed to open.";
+                return 0;
+            } else if (line == task) {
+                break;
+            }
+            lines++;
+        }
+        // WHILE going through every line in the json file, increment a counter. IF task equals line, display the increment counter(ID)
+        
+        // OUTPUT "Task added successfully " along with an ID number for the task
+        std::cout << "Task added successfully (ID: " << lines << ")\n";
+
+    } else if (action == "update") {
+
+    } else if (action == "delete") {
 
     }
-
+return 0;
 }
