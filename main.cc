aca7b492ccced4f54b6053c2b16a9fc0d9e3a9d0
Aman Khajuria <24khajuriaa@gmail.com>
2025-06-08 15:54:14 -0700

    Changes to main.cc. Added json.cc & .h
    Was parsing information into the json file the wrong way. Restarted the add function, made some headway but got stuck at adding tasks back to back. The new file will be used to store all the parsing functions when converting to json.

--- main.cc
+++ main.cc
@@ -2,35 +2,53 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <cstdlib>
+
+
+// Struct for holding data members related to task, and functions
+struct Task {
+    std::string description;
+    int id = 1;
+    std::string taskToJson() {
+        return "  {\n id} ";
+    
+    }
+};
+
 
 int main(int argc, char* argv[]) {
 
 
-    // CHANGE c strings into a string
-
+    // CHANGE c strings into a string; Store general command line actions here
     std::string action = argv[1];
     std::string task = argv[2];
+
+    // MOVED variables outside of IF add scope to make global variables
+    std::fstream file{"tasks.json", std::ios::in | std::ios::out | std::ios::app};
+    std::string line;
+    int Id = 1;
+
+    Task t;
 
     // NOTE: Instructions in the README.md file will explain how to use the program
 
     // IF commandline argument EQUALS "add", or "update", or "delete", run something
     if (action == "add") {
-        // Create an input stream to a json file
-        std::fstream file{"tasks.json", std::ios::in | std::ios::out | std::ios::app};
 
         // IF file is open, store task in it, else throw an error
         if (file.is_open()) {
-            file << task;
-            file << "\n";
+            file  << task;
+            file << '\n';
         } else {
             std::cerr << "Error: File failed to open";
         }
 
         // GET task ID
-        int lines = 1;
-        std::string line;
+       
         // Reset the file cursor position, otherwise the while loop would start at the very end
         file.seekg(0, std::ios::beg);
+
+        // WHILE going through every line in the json file, increment a counter. IF task equals line, display the increment counter(ID)
         while (std::getline(file, line)) {
             if (!file.is_open()) {
                 std::cerr << "Error: File failed to open.";
@@ -38,14 +56,17 @@ int main(int argc, char* argv[]) {
             } else if (line == task) {
                 break;
             }
-            lines++;
+            Id++;
         }
-        // WHILE going through every line in the json file, increment a counter. IF task equals line, display the increment counter(ID)
+
         
         // OUTPUT "Task added successfully " along with an ID number for the task
-        std::cout << "Task added successfully (ID: " << lines << ")\n";
+        std::cout << "Task added successfully (ID: " << Id << ")\n";
 
     } else if (action == "update") {
+        // INIT command line argument to be a number, only for the action "UPDATE"
+
+        // WHILE going through the
 
     } else if (action == "delete") {
 
